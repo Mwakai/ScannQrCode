a8cb477f6c3862923591fb250f33814bcd621092
Alvin Mwakai <mwakaimwambala@gmail.com>
2023-05-31 01:20:44 +0300

    add scan fragment

--- app/src/main/java/com/alvin/scannqrcode/MainActivity.kt
+++ app/src/main/java/com/alvin/scannqrcode/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val scanFragment = ScanFragment()
+
+        supportFragmentManager.beginTransaction().replace(R.id.flFragment, ScanFragment()).commit()
     }
 }
